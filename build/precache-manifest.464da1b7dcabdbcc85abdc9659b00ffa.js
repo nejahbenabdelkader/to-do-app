9a63ec15ad8ea829a703d6d18a749efdd105dec7
Nejah <nejahbenabdelkader@gmail.com>
2021-06-02 18:13:43 +0100

    first commit

---
+++ build/precache-manifest.464da1b7dcabdbcc85abdc9659b00ffa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fa618f21fc0883e2a0ba6c4a270a5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "c4e608c56b4aef7be437",
+    "url": "/static/js/main.85e39e3d.chunk.js"
+  },
+  {
+    "revision": "1db259b82c441a5b05b1",
+    "url": "/static/js/runtime-main.7ac41817.js"
+  }
+]);
